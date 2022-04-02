0ae9284597063c60a8e18e241f59d36727e04f85
Falling42 <3287935289@qq.com>
2022-04-02 08:13:40 +0000

    Deloyed by Github Actions: 2022-04--02 08:13:39

---
+++ sw.js
@@ -0,0 +1,160 @@
+window.addEventListener('load', async () => {
+    navigator.serviceWorker.register(`/sw.js?time=${new Date().getTime()}`)
+        .then(async reg => {
+            //安装成功，建议此处强刷新以立刻执行SW
+            if (window.localStorage.getItem('install') != 'true') {
+                window.localStorage.setItem('install', 'true');
+                setTimeout(() => {
+                    window.location.search = `?time=${new Date().getTime()}`
+                }, 1000)
+            }
+        }).catch(err => {
+            //安装失败，错误信息会由err传参
+        })
+});
+
+const CACHE_NAME = 'ICDNCache';
+let cachelist = [];
+self.addEventListener('install', async function (installEvent) {
+    self.skipWaiting();
+    installEvent.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(cachelist);
+            })
+    );
+});
+self.addEventListener('fetch', async event => {
+    try {
+        event.respondWith(handle(event.request))
+    } catch (msg) {
+        event.respondWith(handleerr(event.request, msg))
+    }
+});
+const handleerr = async (req, msg) => {
+    return new Response(`<h1>CDN分流器遇到了致命错误</h1>
+    <b>${msg}</b>`, { headers: { "content-type": "text/html; charset=utf-8" } })
+}
+let cdn = {
+    "gh": {
+        jsdelivr: {
+            "url": "https://cdn.jsdelivr.net/gh"
+        },
+        jsdelivr_fastly: {
+            "url": "https://fastly.jsdelivr.net/gh"
+        },
+        jsdelivr_gcore: {
+            "url": "https://gcore.jsdelivr.net/gh"
+        },
+        falling42: {
+            "url": "https://jsd.42pic.top/gh"
+        }
+    },
+    "combine": {
+        jsdelivr: {
+            "url": "https://cdn.jsdelivr.net/combine"
+        },
+        jsdelivr_fastly: {
+            "url": "https://fastly.jsdelivr.net/combine"
+        },
+        jsdelivr_gcore: {
+            "url": "https://gcore.jsdelivr.net/combine"
+        },
+        falling42: {
+            "url": "https://jsd.42pic.top/combine"
+        }
+    },
+    "npm": {
+        eleme: {
+            "url": "https://npm.elemecdn.com"
+        },
+        jsdelivr: {
+            "url": "https://cdn.jsdelivr.net/npm"
+        },
+        zhimg: {
+            "url": "https://unpkg.zhimg.com"
+        },
+        unpkg: {
+            "url": "https://unpkg.com"
+        },
+        bdstatic: {
+            "url": "https://code.bdstatic.com/npm"
+        },
+        falling42: {
+            "url": "https://jsd.42pic.top/npm"
+        }
+
+    }
+}
+const handle = async function (req) {
+    const urlStr = req.url
+    const domain = (urlStr.split('/'))[2]
+    let urls = []
+    for (let i in cdn) {
+        for (let j in cdn[i]) {
+            if (domain == cdn[i][j].url.split('https://')[1].split('/')[0] && urlStr.match(cdn[i][j].url)) {
+                urls = []
+                for (let k in cdn[i]) {
+                    urls.push(urlStr.replace(cdn[i][j].url, cdn[i][k].url))
+                }
+                if (urlStr.indexOf('@latest/') > -1) {
+                    return lfetch(urls, urlStr)
+                } else {
+                    return caches.match(req).then(function (resp) {
+                        return resp || lfetch(urls, urlStr).then(function (res) {
+                            return caches.open(CACHE_NAME).then(function (cache) {
+                                cache.put(req, res.clone());
+                                return res;
+                            });
+                        });
+                    })
+                }
+            }
+        }
+    }
+    return fetch(req)
+}
+const lfetch = async (urls, url) => {
+    let controller = new AbortController();
+    const PauseProgress = async (res) => {
+        return new Response(await (res).arrayBuffer(), { status: res.status, headers: res.headers });
+    };
+    if (!Promise.any) {
+        Promise.any = function (promises) {
+            return new Promise((resolve, reject) => {
+                promises = Array.isArray(promises) ? promises : []
+                let len = promises.length
+                let errs = []
+                if (len === 0) return reject(new AggregateError('All promises were rejected'))
+                promises.forEach((promise) => {
+                    promise.then(value => {
+                        resolve(value)
+                    }, err => {
+                        len--
+                        errs.push(err)
+                        if (len === 0) {
+                            reject(new AggregateError(errs))
+                        }
+                    })
+                })
+            })
+        }
+    }
+    return Promise.any(urls.map(urls => {
+        return new Promise((resolve, reject) => {
+            fetch(urls, {
+                signal: controller.signal
+            })
+                .then(PauseProgress)
+                .then(res => {
+                    if (res.status == 200) {
+                        controller.abort();
+                        resolve(res)
+                    } else {
+                        reject(res)
+                    }
+                })
+        })
+    }))
+}
